e39a895879fd93a4386ac8f49e348d9f6a784227
shazampanthenon31293 <47402498+shazampanthenon31293@users.noreply.github.com>
2021-10-01 09:50:59 -0400

    22

--- sean.js
+++ sean.js
@@ -1,3 +1,12 @@
 function s1(){
-    
+    var s1 = "Random Number Generator"
+    document.getElementById("s2").innerHTML= s1;
+    var minnumber = document.getElementById("s4").value;
+    var maxnumber = document.getElementById("s5").value;
+    var randomnumber = Math.floor(Math.random() * 100) + 1;
+    document.getElementById("s2").innerHTML += "<br>";
+    document.getElementById("s2").innerHTML += randomnumber;
+}
+function s2(){
+    document.getElementById("s2").innerHTML= "";
 }
